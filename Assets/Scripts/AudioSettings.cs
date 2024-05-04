1ce9c968d212725a5ac12bab884c109eb0f33959
Archie Ivanov <arnix4@gmail.com>
2024-05-04 11:47:39 +0300

    Music

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class AudioSettings : MonoBehaviour
 {
-
     FMOD.Studio.EventInstance SFXVolumeTestEvent;
 
     FMOD.Studio.Bus Music;
     FMOD.Studio.Bus SFX;
     FMOD.Studio.Bus Master;
-    float MusicVolume = 0.5f;
-    float SFXVolume = 0.5f;
-    float MasterVolume = 1f;
+    private float MusicVolume = 0.0f;
+    private float SFXVolume = 0.5f;
+    private float MasterVolume = 1f;
 
     void Awake()
     {
