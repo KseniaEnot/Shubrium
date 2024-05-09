a1e4e6d669055b902f591805fe2f543f8c552a96
Archie Ivanov <arnix4@gmail.com>
2024-05-09 12:06:09 +0300

    TestPlayerMove

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private Rigidbody _rb;
+
+    private bool _grounded;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            MovePlayer();
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && _grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            _grounded = true;
+        }
+    }
+
+    private void Jump()
+    {
+        _grounded = false;
+        _rb.velocity = new Vector3(0, _jumpForce, 0);
+    }
+
+    private void MovePlayer()
+    {
+        float direction = Input.GetAxis("Horizontal");
+
+        if(direction > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (direction < 0) 
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+        float step = _speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + direction * step, transform.position.y), step);
+    }
+}
--- Assets/Scripts/Trash/ButtonSound.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ButtonSound : MonoBehaviour
-{
-    public bool SoundOn => _soundOn;
-
-    private bool _soundOn = false;
-
-}
